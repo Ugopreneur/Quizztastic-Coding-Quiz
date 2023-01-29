3d714584a9625511647be11986cd644004b59042
ugopreneur <ugnwokoro@yahoo.com>
2023-01-29 14:38:20 +0100

    fixed bug in scores.js that was causing highscores not to populate

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,18 +1,20 @@
+// fetch scores from local storage
+var scoreBoardString = localStorage.getItem("scoreBoardString");
 
+// convert the string of scores gotten from local storage into an array
+var allScoresArray = JSON.parse(scoreBoardString);
 
+// determine where score board will be built in user's view
 var scoreBoard = document.querySelector("#highscores");
 
-
-
-
-
-for (i = 0; i < scoreBoardArray.length; i++) {
+// build the scoreboard by looping through the array of scores from local storage
+for (i = 0; i < allScoresArray.length; i++) {
 
     var scoreBoardListItem = document.createElement("li");
-    scoreBoardListItem.textContent = scoreBoardArray[i].playerInitials + " - " + scoreBoardArray[i].playerScore;
+    scoreBoardListItem.textContent = allScoresArray[i].playerInitials + " - " + allScoresArray[i].playerScore;
     scoreBoard.appendChild(scoreBoardListItem);
 
-}
+};
 
 
 
